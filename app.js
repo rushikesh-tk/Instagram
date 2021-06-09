f6f14e3626e1d2ce4bf6e4b3210defd97cf10b9b
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2021-06-09 15:53:07 +0530

    add rate limiter

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const rateLimit = require("express-rate-limit");
 const { MONGOURI } = require("./config/keys.js");
 
 mongoose.connect(MONGOURI, {
@@ -21,9 +22,15 @@ mongoose.connection.on("error", (err) => {
 require("./models/user");
 require("./models/post");
 
+var limiter = new rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 5,
+});
+
 app.use(express.json());
 app.use(cors());
 
+app.use("/", limiter);
 app.use(require("./routes/auth"));
 app.use(require("./routes/post"));
 app.use(require("./routes/user"));

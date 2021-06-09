c091c2a93e6d38a28f14017abce8b90db78c0853
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2021-06-09 15:27:26 +0530

    minor change

--- routes/post.js
+++ routes/post.js
@@ -1,126 +1,140 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
-const Post = mongoose.model('Post');
-const requireLogin = require('../middleware/requireLogin');
+const Post = mongoose.model("Post");
+const requireLogin = require("../middleware/requireLogin");
 
-router.get('/allposts',requireLogin, (req, res) => {
-    Post.find()
-    .populate("postedBy","_id name")
-    .populate("comments.postedBy", "_id name")
-    .sort('-createdAt')
-    .then(posts => {
-        res.json({posts});
-    })
-    .catch(err => {
-        console.log(err);
-    })
-})
-
-router.get('/mypost',requireLogin, (req, res) => {
-
-    Post.find({postedBy:req.user._id})
+router.get("/allposts", requireLogin, (req, res) => {
+  Post.find()
     .populate("postedBy", "_id name")
-    .then(result => {
-        //console.log(result);
-        res.json({MyPosts:result});
+    .populate("comments.postedBy", "_id name")
+    .sort("-createdAt")
+    .then((posts) => {
+      res.json({ posts });
     })
-    .catch(err => {
-        console.log("Error:"+ err );
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/mypost", requireLogin, (req, res) => {
+  Post.find({ postedBy: req.user._id })
+    .populate("postedBy", "_id name")
+    .then((result) => {
+      //console.log(result);
+      res.json({ MyPosts: result });
     })
-})
+    .catch((err) => {
+      console.log("Error:" + err);
+    });
+});
 
-router.post('/createpost',requireLogin, (req, res) => {
-    const {title, body, photo} = req.body;
+router.post("/createpost", requireLogin, (req, res) => {
+  const { title, body, photo } = req.body;
 
-    if(!title || !body || !photo){
-        return res.status(422).json({error:"Please Add All The Fields"});
+  if (!title || !body || !photo) {
+    return res.status(422).json({ error: "Please Add All The Fields" });
+  }
+
+  req.user.password = undefined;
+
+  const post = new Post({
+    title,
+    body,
+    photo,
+    postedBy: req.user,
+  });
+
+  post
+    .save()
+    .then((result) => {
+      //console.log(result);
+      res.json({ post: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.put("/like", requireLogin, (req, res) => {
+  Post.findByIdAndUpdate(
+    req.body.postId,
+    {
+      $push: { likes: req.user._id },
+    },
+    {
+      new: true,
     }
-
-    req.user.password = undefined;
-
-    const post = new Post({
-        title,
-        body,
-        photo,
-        postedBy: req.user
-    })
-
-    post.save()
-    .then(result => {
-        //console.log(result);
-        res.json({post:result});
-    })
-    .catch(err => {
-        console.log(err);
-    })
-})
-
-router.put('/like', requireLogin, (req, res) => {
-    Post.findByIdAndUpdate(req.body.postId, {
-        $push:{likes:req.user._id}
-    },{
-        new:true
-    }).exec((err, result) => {
-        if(err){
-            return res.status(422).json({error:err})
-        } else {
-            res.json(result)
-        }
-    })
-})
-
-router.put('/unlike', requireLogin, (req, res) => {
-    Post.findByIdAndUpdate(req.body.postId, {
-        $pull: {likes: req.user._id}
-    }, {
-        new:true
-    }).exec((err, result) => {
-        if (err) {
-            return res.status(422).json({ error: err })
-        } else {
-            res.json(result)
-        }
-    })
-})
-
-router.put('/comment', requireLogin, (req, res) => {
-    const comment = {
-        text: req.body.text,
-        postedBy: req.user._id
+  ).exec((err, result) => {
+    if (err) {
+      return res.status(422).json({ error: err });
+    } else {
+      res.json(result);
     }
-    Post.findByIdAndUpdate(req.body.postId, {
-        $push:{comments:comment}
-    },{
-        new:true
-    })
+  });
+});
+
+router.put("/unlike", requireLogin, (req, res) => {
+  Post.findByIdAndUpdate(
+    req.body.postId,
+    {
+      $pull: { likes: req.user._id },
+    },
+    {
+      new: true,
+    }
+  ).exec((err, result) => {
+    if (err) {
+      return res.status(422).json({ error: err });
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+router.put("/comment", requireLogin, (req, res) => {
+  const comment = {
+    text: req.body.text,
+    postedBy: req.user._id,
+  };
+  Post.findByIdAndUpdate(
+    req.body.postId,
+    {
+      $push: { comments: comment },
+    },
+    {
+      new: true,
+    }
+  )
     .populate("comments.postedBy", "_id name")
     .populate("postedBy", "_id name")
     .exec((err, result) => {
-        if(err){
-            return res.status(422).json({error:err})
-        } else {
-            res.json(result)
-        }
-    })
-})
+      if (err) {
+        return res.status(422).json({ error: err });
+      } else {
+        res.json(result);
+      }
+    });
+});
 
-router.delete('/deletepost/:postId', requireLogin, (req, res) => {
-    Post.findOne({_id : req.params.postId})
+router.delete("/deletepost/:postId", requireLogin, (req, res) => {
+  Post.findOne({ _id: req.params.postId })
     .populate("postedBy", "_id")
-    .exec((err ,post) => {
-        if(err || !post){
-            return res.status(422).json({error: err})
-        }
-        if(post.postedBy._id.toString() === req.user._id.toString()){
-            post.remove()
-            .then(result => {
-                res.json(result)
-            }).catch(err => {
-                console.log(err)
-            })
-        }
-    })
-})
+    .exec((err, post) => {
+      if (err || !post) {
+        return res.status(422).json({ error: err });
+      }
+      if (post.postedBy._id.toString() === req.user._id.toString()) {
+        post
+          .remove()
+          .then((result) => {
+            res.json(result);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    });
+});
 
 module.exports = router;
